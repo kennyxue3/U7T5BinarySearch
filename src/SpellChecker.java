5f1d2a46cb99697e047373d72709b93602149328
kenny_xue3 <kennyx3@nycstudents.net>
2022-02-16 13:38:01 -0500

    dasas

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -4,85 +4,90 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class SpellChecker
-{
-  private ArrayList<String> dictionary;
+public class SpellChecker {
+    private ArrayList<String> dictionary;
 
-  // constructor; uses try-catch syntax which we haven't discussed!
-  public SpellChecker()
-  {
-    importDictionary();
-  }
+    // constructor; uses try-catch syntax which we haven't discussed!
+    public SpellChecker() {
+        importDictionary();
+    }
 
-  public ArrayList<String> getDictionary()
-  {
-    return dictionary;
-  }
+    public ArrayList<String> getDictionary() {
+        return dictionary;
+    }
 
-  /** This uses LINEAR search to find a word in the dictionary ArrayList and also
-    * prints out the number of words checked.
-    *
-    * Instead of returning the index the word is found, it simply returns TRUE
-    * if the word is found, and FALSE otherwise.
-  */
-  public boolean linearSpellCheck(String word)
-  {
-    int numChecks = 0;
-    
-    for(int i=0; i < dictionary.size(); i++)
-    {
-      numChecks++;
-      
-      if (word.equals(dictionary.get(i)))
-      {
+    /**
+     * This uses LINEAR search to find a word in the dictionary ArrayList and also
+     * prints out the number of words checked.
+     * <p>
+     * Instead of returning the index the word is found, it simply returns TRUE
+     * if the word is found, and FALSE otherwise.
+     */
+    public boolean linearSpellCheck(String word) {
+        int numChecks = 0;
+
+        for (int i = 0; i < dictionary.size(); i++) {
+            numChecks++;
+
+            if (word.equals(dictionary.get(i))) {
+                System.out.println("-- LINEAR SEARCH: Number of words checked (loops/runtime): " + numChecks);
+                return true;
+            }
+        }
+
         System.out.println("-- LINEAR SEARCH: Number of words checked (loops/runtime): " + numChecks);
-        return true;
-      }
+        return false;
     }
 
-    System.out.println("-- LINEAR SEARCH: Number of words checked (loops/runtime): " + numChecks);
-    return false;
-  }
-  
-  /** This uses BINARY search to find a word in the dictionary ArrayList and also
-    * prints out the number of words checked.
-    *
-    * Instead of returning the index the word is found, it simply returns TRUE
-    * if the word is found, and FALSE otherwise.
-  */
-  public boolean binarySpellCheck(String word)
-  {
-    /* IMPLEMENT ME! */
+    /**
+     * This uses BINARY search to find a word in the dictionary ArrayList and also
+     * prints out the number of words checked.
+     * <p>
+     * Instead of returning the index the word is found, it simply returns TRUE
+     * if the word is found, and FALSE otherwise.
+     */
+    public boolean binarySpellCheck(String word) {
+        int leftIdx = 0;
+        int rightIdx = dictionary.size() - 1;
+        int count = 0;
 
-    return false; // STUB
-  }
-
-  // private helper method, called in the constructor, which loads the words
-  // from the dictionary.txt text file into the "dictionary" instance variable!
-  private void importDictionary()
-  {
-    String[] tmp = null;
-    try
-    {
-      FileReader fileReader = new FileReader("src\\dictionary.txt");
-      BufferedReader bufferedReader = new BufferedReader(fileReader);
-      ArrayList<String> lines = new ArrayList<String>();
-      String line = null;
-
-      while ((line = bufferedReader.readLine()) != null)
-      {
-        lines.add(line);
-      }
-
-      bufferedReader.close();
-      tmp = lines.toArray(new String[lines.size()]);
-      System.out.println("\nFile imported successfully!");
-    }
-    catch (IOException e)
-    {
-      System.out.println("Error importing file; unable to access "+ e.getMessage());
+        while (leftIdx <= rightIdx) {
+            count++;
+            int center = (leftIdx + rightIdx) / 2;
+            if (dictionary.get(center).compareTo(word) > 0) {
+                rightIdx = center - 1;
+            } else if (dictionary.get(center).compareTo(word) < 0) {
+                leftIdx = center + 1;
+            } else {
+                System.out.println("-- BINARY SEARCH: Number of words checked (loops/runtime): " + count);
+                return true;
+            }
+        }
+        System.out.println("-- BINARY SEARCH: Number of words checked (loops/runtime): " + count);
+        return false;
     }
 
-    dictionary = new ArrayList<String>(Arrays.asList(tmp));
-  }
+    // private helper method, called in the constructor, which loads the words
+    // from the dictionary.txt text file into the "dictionary" instance variable!
+    private void importDictionary() {
+        String[] tmp = null;
+        try {
+            FileReader fileReader = new FileReader("src\\dictionary2.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ArrayList<String> lines = new ArrayList<String>();
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            bufferedReader.close();
+            tmp = lines.toArray(new String[lines.size()]);
+            System.out.println("\nFile imported successfully!");
+        } catch (IOException e) {
+            System.out.println("Error importing file; unable to access " + e.getMessage());
+        }
+
+        dictionary = new ArrayList<String>(Arrays.asList(tmp));
+    }
 }
